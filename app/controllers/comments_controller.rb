0997b29fa29dc283d592c276495f9658d09a377c
cep104 <candicelizabethpeters@gmail.com>
2021-01-06 23:21:47 -0600

    commit tree 58a8359c8d012f5bb7cdec30889d9428e951feb9

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,13 @@
-require "pry"
-
 class CommentsController < ApplicationController
+
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
 
-  def user=(user)
-    self.user = User.find_or_create_by(name: user[:username])
-    self.user.update(user)
-  end
-
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, users_attributes: [:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
-  accepts_nested_attributes_for :user, reject_if: :reject_user
-
-  def reject_user(attributes)
-    attributes.each do |key, value|
-      value.blank?
-    end
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
   end
+
 end
+
